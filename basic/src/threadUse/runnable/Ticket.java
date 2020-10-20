dbd8402df511fe8172e0ae074ff42205963a3de3
liuya <1093099339@qq.com>
2020-10-20 23:56:30 +0800

    并发问题实现

--- basic/src/threadUse/runnable/Ticket.java
+++ basic/src/threadUse/runnable/Ticket.java
@@ -8,7 +8,7 @@ package threadUse.runnable;
  **/
 public class Ticket implements Runnable {
 
-    private int ticketNumbers = 10;
+    private  int ticketNumbers = 15;
 
     @Override
     public void run() {
@@ -17,19 +17,33 @@ public class Ticket implements Runnable {
             if (ticketNumbers <= 0){
                 break;
             }
+
+            //模拟延时
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             System.out.println(Thread.currentThread().getName()+"-->抢到了第"+ticketNumbers--+"票");
         }
 
     }
-}
 
-class  Main4{
     public static void main(String[] args) {
 
-        new Thread(new Ticket(),"骄傲").start();
-        new Thread(new Ticket(),"嫉妒").start();
-        new Thread(new Ticket(),"贪婪").start();
-        new Thread(new Ticket(),"暴食").start();
+        //一个资源，让四个线程去抢
+        //这就是并发
+        Ticket ticket = new Ticket();
+
+        new Thread(ticket,"骄傲").start();
+        new Thread(ticket,"嫉妒").start();
+        new Thread(ticket,"暴怒").start();
+        new Thread(ticket,"懒惰").start();
+        new Thread(ticket,"贪婪").start();
+        new Thread(ticket,"暴食").start();
+        new Thread(ticket,"色欲").start();
+
 
     }
 }
