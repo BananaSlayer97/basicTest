9779cd3bc7c8013f9b85a751ddebe5b765ca044f
liuya <1093099339@qq.com>
2020-10-21 11:03:09 +0800

    Lambda相当于匿名内部类加上自定义方法

--- basicStuty/src/lambda/LambdaByArrow.java
+++ basicStuty/src/lambda/LambdaByArrow.java
@@ -9,9 +9,12 @@ package lambda;
 public class LambdaByArrow {
 
     public static void main(String[] args) {
-        Car car = new Bike();
 
-        car = () ->{
+        /**
+         * 直接lambda 相当于 实现类，内部实现类
+         * 里面就是要实现的代码
+         */
+        Car car = () ->{
             for (int i = 0; i < 20; i++) {
                 System.out.println(i);
             }
@@ -19,6 +22,10 @@ public class LambdaByArrow {
 
         car.name();
 
+        /**
+         * 普通调用方法，需要写个实现类
+         * 然后再调用方法
+         */
         Car car2 = new Bike();
         car2.name();
     }
@@ -29,6 +36,9 @@ interface  Car{
     void name();
 }
 
+/**
+ * 接口实现类
+ */
 class  Bike implements Car{
     @Override
     public void name() {}
