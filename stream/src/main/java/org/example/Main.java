ab6c6fb321a5d11b5a68a35a83512fe323758272
liuya <1039099339@qq.com>
2022-12-15 13:24:52 +0800

    得到一个列表中最长的元素和最短的元素

--- stream/src/main/java/org/example/Main.java
+++ stream/src/main/java/org/example/Main.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import cn.hutool.core.lang.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+        List<String> stringList = Arrays.asList("Java 8", "Lambdas", "In", "Action");
 
+        Optional<Integer> min = stringList.stream()
+                .map(String::length)
+                .reduce(Integer::min);
+
+        Optional<Integer> max = stringList.stream()
+                .map(String::length)
+                .reduce(Integer::max);
+
+        Console.log(max.get());
+        Console.log(min.get());
 
     }
 }
